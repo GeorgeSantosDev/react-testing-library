caeda8653c78dee5ec77fbbcb388646523d1c728
George Santos <george.santos_ufmg@outlook.com>
2022-08-16 16:02:36 -0300

    testando se é renderizado as informações da pokedex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Testing about Page', () => {
+  it('should have a Pokedex information', () => {
+    renderWithRouter(<About />);
+    const pokedexInformation = screen.getByText(/This application simulates a Pokédex/i);
+
+    expect(pokedexInformation).toBeDefined();
+  });
+});
