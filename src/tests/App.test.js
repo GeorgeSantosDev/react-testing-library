64b0d2584785df5e922abd03cb8226a38c4332c3
George Santos <george.santos_ufmg@outlook.com>
2022-08-16 15:15:06 -0300

    Testando os elementos de link

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Testing the App component', () => {
+  it('should have elements with texts: Home, About and Favorite Pokémons', () => {
+    const home = screen.getByRole('link', { name: 'Home' });
+    const about = screen.getByRole('link', { name: 'About' });
+    const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(home).toBeDefined();
+    expect(about).toBeDefined();
+    expect(favoritePokemons).toBeDefined();
+  });
+});
