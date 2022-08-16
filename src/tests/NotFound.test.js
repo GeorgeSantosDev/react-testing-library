a38671adcbcd109587ede501dab7f9c1c2005413
George Santos <george.santos_ufmg@outlook.com>
2022-08-16 17:59:45 -0300

    Testando se a página Not Found retorna um texto e uma imagem

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testing Not Found Page', () => {
+  it('should have heading h2 with the text Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', { name: /Page requested not found/i });
+
+    expect(h2).toBeDefined();
+  });
+  it('should have an image with an specific URL', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img', { name: /Pikachu crying/i });
+
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
