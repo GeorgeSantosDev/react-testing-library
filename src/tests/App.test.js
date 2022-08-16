84cc9b60dc596e0732455ad879fb5055299c1d59
George Santos <george.santos_ufmg@outlook.com>
2022-08-16 15:36:35 -0300

    testando o redirecionamento para home e criando o helper renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testing the App component', () => {
   it('should have elements with texts: Home, About and Favorite Pokémons', () => {
+    renderWithRouter(<App />);
     const home = screen.getByRole('link', { name: 'Home' });
     const about = screen.getByRole('link', { name: 'About' });
     const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
@@ -12,5 +14,13 @@ describe('Testing the App component', () => {
     expect(home).toBeDefined();
     expect(about).toBeDefined();
     expect(favoritePokemons).toBeDefined();
+  });
+  it('should redirect to Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+
+    userEvent.click(home);
+
+    expect(history.location.pathname).toBe('/');
   });
 });
